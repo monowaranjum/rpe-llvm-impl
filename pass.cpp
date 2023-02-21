f1fcb218dc1df4140b91936e9b486402f4c18ebd
monowar-1994 <monowaranjum@gmail.com>
2023-02-21 04:35:09 -0500

    Temporary commit, adding path traversal

--- pass.cpp
+++ pass.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <list>
 #include <utility>
-#include <cstring>
+#include <string>
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
@@ -17,127 +18,150 @@
 #include <jsoncpp/json/json.h>
 
 using namespace llvm;
+using namespace std;
 
 namespace
 {
 
-    class AugmentedBasicBlock{
+    class AugmentedBasicBlock
+    {
     private:
-        std::string blockId; // Unique Block Id
-        bool isRootBlock; // Defines if this is the starting block of the function. 
-        bool isConditionalBlock; // Does it have branching at the end or just a straight jump to next block
-        bool hasInlineAssembly; // Is there any inline assembly instruction? Then parse separately
-        std::string trueBlock;  // If branched, then next true block
-        std::string falseBlock; // If branched, then next false block
-        std::string nextBlock; // If not branched, then next block
-        std::vector<std::string> instructions; // All the call instructions are stored here (operation and arguments)
-        std::vector<StringRef> functions; // All the functions are stored here (Name only)
-        std::vector<std::string> parents; // Can keep track of the parent blocks if implementation wants        
+        string blockId;                   // Unique Block Id
+        bool isRootBlock;                      // Defines if this is the starting block of the function.
+        bool isConditionalBlock;               // Does it have branching at the end or just a straight jump to next block
+        bool hasInlineAssembly;                // Is there any inline assembly instruction? Then parse separately
+        string trueBlock;                 // If branched, then next true block
+        string falseBlock;                // If branched, then next false block
+        string nextBlock;                 // If not branched, then next block
+        vector<string> instructions; // All the call instructions are stored here (operation and arguments)
+        vector<StringRef> functions;      // All the functions are stored here (Name only)
+        vector<string> parents;      // Can keep track of the parent blocks if implementation wants
     public:
-
-        AugmentedBasicBlock(){
+        AugmentedBasicBlock()
+        {
             isConditionalBlock = false;
             hasInlineAssembly = false;
             isRootBlock = false;
         }
-        
-        std::string getBlockId(){
+
+        string getBlockId()
+        {
             return blockId;
         }
-        void setBlockId(std::string blockID){
+        void setBlockId(string blockID)
+        {
             blockId = blockID;
         }
 
-        void setRootBlock(){
+        void setRootBlock()
+        {
             isRootBlock = true;
         }
-        bool isARootBlock(){
+        bool isARootBlock()
+        {
             return isRootBlock;
         }
-        void setInlineAssembly(){
+        void setInlineAssembly()
+        {
             hasInlineAssembly = true;
         }
-        bool getInlineAssemblyStatus(){
+        bool getInlineAssemblyStatus()
+        {
             return hasInlineAssembly;
         }
-        void setConditionalBlock(){
+        void setConditionalBlock()
+        {
             isConditionalBlock = true;
         }
-        bool getConditionalBlock(){
+        bool getConditionalBlock()
+        {
             return isConditionalBlock;
         }
-        std::string getNextBlock(){
+        string getNextBlock()
+        {
             return nextBlock;
         }
-        void setNextBlock(std::string nextB){
+        void setNextBlock(string nextB)
+        {
             nextBlock = nextB;
         }
-        std::string getTrueBlock(){
+        string getTrueBlock()
+        {
             return trueBlock;
         }
-        void setTrueBlock(std::string trueB){
+        void setTrueBlock(string trueB)
+        {
             trueBlock = trueB;
         }
-        std::string getFalseBlock(){
+        string getFalseBlock()
+        {
             return falseBlock;
         }
-        void setFalseBlock(std::string falseB){
+        void setFalseBlock(string falseB)
+        {
             falseBlock = falseB;
         }
 
-        std::vector<std::string> getInstructions(){
+        vector<string> getInstructions()
+        {
             return instructions;
         }
-        void addInstruction(std::string instruction){
+        void addInstruction(string instruction)
+        {
             instructions.push_back(instruction);
         }
-        std::vector<StringRef> getFunctions(){
+        vector<StringRef> getFunctions()
+        {
             return functions;
         }
-        void addFunction(StringRef functionName){
+        void addFunction(StringRef functionName)
+        {
             functions.push_back(functionName);
         }
-        std::vector<std::string> getParents(){
+        vector<string> getParents()
+        {
             return parents;
         }
-        void addParent(std::string parent){
+        void addParent(string parent)
+        {
             parents.push_back(parent);
         }
     };
 
-    static std::string getSimpleNodeLabel(const BasicBlock &Node)
+    static string getSimpleNodeLabel(const BasicBlock &Node)
     { // Copied from Github
         if (!Node.getName().empty())
             return Node.getName().str();
 
-        std::string Str;
+        string Str;
         raw_string_ostream OS(Str);
 
         Node.printAsOperand(OS, false);
         return OS.str();
     }
 
-    static std::string getSimpleNodeLabel(const BasicBlock *Node)
+    static string getSimpleNodeLabel(const BasicBlock *Node)
     { // Pointer Version
         if (!Node->getName().empty())
             return Node->getName().str();
 
-        std::string Str;
+        string Str;
         raw_string_ostream OS(Str);
 
         Node->printAsOperand(OS, false);
         return OS.str();
     }
 
-
-    static std::string getInstructionString(Instruction *inst){
-        std::string s;
+    static string getInstructionString(Instruction *inst)
+    {
+        string s;
         raw_string_ostream ss(s);
-        ss<<*inst;
+        ss << *inst;
         return ss.str();
     }
 
-    static void parseCallInstruction(CallInst *call, Instruction *inst, AugmentedBasicBlock *currBlock){
+    static void parseCallInstruction(CallInst *call, Instruction *inst, AugmentedBasicBlock *currBlock)
+    {
         // assert(call != NULL);
         // int numOperands = call->getNumOperands();
         // for(int i=0;i<numOperands-1;i++){
@@ -145,47 +169,84 @@ namespace
         //     operand->print(errs(), false);
         //     errs()<<"\n";
         // }
-        if(call->isInlineAsm()){
+        if (call->isInlineAsm())
+        {
             currBlock->setInlineAssembly();
-            currBlock->addInstruction(getInstructionString(inst));           
+            currBlock->addInstruction(getInstructionString(inst));
         }
-        else{
+        else
+        {
             currBlock->addInstruction(getInstructionString(inst));
             Function *function = call->getCalledFunction();
-            if(function != NULL){
+            if (function != NULL)
+            {
                 currBlock->addFunction(function->getName());
             }
-            else{
-                errs()<<"ERROR: function from call instruction is null. Can be a function pointer.\n";
+            else
+            {
+                errs() << "ERROR: function from call instruction is null. Might be a function pointer.\n";
             }
         }
     }
 
-    static void parseBinaryBranchInstruction(BranchInst *brInst, AugmentedBasicBlock *acfgNode){
-        if(brInst->isConditional()){
+    static void parseBinaryBranchInstruction(BranchInst *brInst, AugmentedBasicBlock *acfgNode)
+    {
+        if (brInst->isConditional())
+        {
             acfgNode->setConditionalBlock();
-            Value *op1 = brInst->getSuccessor(0);                                
+            Value *op1 = brInst->getSuccessor(0);
             acfgNode->setTrueBlock(getSimpleNodeLabel(dyn_cast<BasicBlock>(op1)));
-            Value *op2 = brInst->getSuccessor(1);                                
+            Value *op2 = brInst->getSuccessor(1);
             acfgNode->setFalseBlock(getSimpleNodeLabel(dyn_cast<BasicBlock>(op2)));
         }
-        else if(brInst->isUnconditional()){
+        else if (brInst->isUnconditional())
+        {
             Value *op = brInst->getSuccessor(0);
             acfgNode->setNextBlock(getSimpleNodeLabel(dyn_cast<BasicBlock>(op)));
         }
     }
 
-    static void parseSwitchInstruction(SwitchInst *switch_inst){
+    static void parseSwitchInstruction(SwitchInst *switch_inst)
+    {
         // [TODO]
     }
 
-    
-    
+    static void printEdgeList(vector<pair<string, string>> eList)
+    {
+        for (pair<string, string> edge : eList)
+        {
+            errs() << edge.first << " -> " << edge.second << "\n";
+        }
+    }
+
+    static vector<list<string>> extractPaths(vector<pair<string, string>> eList, string rootId)
+    {
+        vector<list<string>> paths;
+        map<string, vector<string>> adjacencyList; 
+        for( auto it = begin(eList); it != end(eList); ++it ){
+            pair<string, string> temp = *it;
+            if(adjacencyList.find(temp.first) == adjacencyList.end() ){
+                vector<string> children;
+                children.push_back(temp.second);
+                adjacencyList[temp.first] = children;
+            }
+            else{
+                vector<string> currChildren = adjacencyList[temp.first];
+                currChildren.push_back(temp.second);
+                adjacencyList[temp.first] = currChildren;
+            }
+        }
+        // Traverse the graph by DFS
+
+
+        // Traversal ends
+        return paths;
+    }
+
     struct BasicBlockExtractionPass : public ModulePass
     {
         static char ID;
         BasicBlockExtractionPass() : ModulePass(ID){};
-        
 
         virtual bool runOnModule(Module &M)
         {
@@ -193,81 +254,96 @@ namespace
             {
                 const Function &currentFunction = *functionIt;
                 errs() << "Current Function: " << currentFunction.getName() << "\n";
-                std::map<std::string, AugmentedBasicBlock> idAcfgNode;
-                std::map<std::string, std::string> edgeList;
-                
-                if(currentFunction.getBasicBlockList().size() == 0){
+                map<string, AugmentedBasicBlock> idAcfgNode;
+                vector<pair<string, string>> edgeList;
+
+                if (currentFunction.getBasicBlockList().size() == 0)
+                {
                     continue;
                 }
-                // currentFunction.viewCFG();
+                currentFunction.viewCFG();
                 for (auto &basicBlock : currentFunction)
                 {
-                    AugmentedBasicBlock acfgNode; 
+                    AugmentedBasicBlock acfgNode;
 
-                    const BasicBlock *rootBlock = NULL;  
+                    const BasicBlock *rootBlock = NULL;
                     const BasicBlock *predecessor = NULL;
-                    std::vector<BasicBlock *> predecessorNodes;
+                    vector<BasicBlock *> predecessorNodes;
 
                     bool isRootBlock = false;
                     bool hasUniquePredecessor = false;
                     bool hasMultiplePredecessors = false;
 
-                    
-                    acfgNode.setBlockId(getSimpleNodeLabel(basicBlock)); 
-                    
+                    acfgNode.setBlockId(getSimpleNodeLabel(basicBlock));
+
                     if (basicBlock.hasNPredecessors(1))
                     {
                         predecessor = basicBlock.getUniquePredecessor();
                         hasUniquePredecessor = true;
-                        acfgNode.addParent(getSimpleNodeLabel(predecessor));
+                        string parentName = getSimpleNodeLabel(predecessor);
+                        acfgNode.addParent(parentName);
+                        edgeList.push_back(make_pair(parentName, acfgNode.getBlockId()));
                     }
-                    else if (basicBlock.hasNPredecessorsOrMore(2)){
+                    else if (basicBlock.hasNPredecessorsOrMore(2))
+                    {
                         BasicBlock *pointerToCurrentBlock = const_cast<BasicBlock *>(&basicBlock);
-                        if(pointerToCurrentBlock != NULL){
-                            for( BasicBlock *predecessor: predecessors(pointerToCurrentBlock) ){
-                                acfgNode.addParent(getSimpleNodeLabel(predecessor));
-                                predecessorNodes.push_back(predecessor);    
+                        if (pointerToCurrentBlock != NULL)
+                        {
+                            for (BasicBlock *predecessor : predecessors(pointerToCurrentBlock))
+                            {
+                                string tempParent = getSimpleNodeLabel(predecessor);
+                                acfgNode.addParent(tempParent);
+                                predecessorNodes.push_back(predecessor);
+                                edgeList.push_back(make_pair(tempParent, acfgNode.getBlockId()));
                             }
                             hasMultiplePredecessors = true;
                         }
-                        else{
-                            errs()<<"ERROR: The const to non-const conversion may have gone wrong.\n";
+                        else
+                        {
+                            errs() << "ERROR: The const to non-const conversion may have gone wrong.\n";
                             continue;
                         }
                     }
-                    else{
+                    else
+                    {
                         rootBlock = const_cast<BasicBlock *>(&basicBlock);
                         isRootBlock = true;
                         acfgNode.setRootBlock();
                     }
-        
-                    if(isRootBlock){
+
+                    if (isRootBlock)
+                    {
                         acfgNode.setRootBlock();
                     }
 
                     for (auto &instruction : basicBlock)
                     {
-                        if(isa<CallInst>(instruction)){
+                        if (isa<CallInst>(instruction))
+                        {
                             Instruction *inst = const_cast<Instruction *>(&instruction);
                             CallInst *call = dyn_cast<CallInst>(inst);
-                            parseCallInstruction(call, inst, &acfgNode);                        
+                            parseCallInstruction(call, inst, &acfgNode);
                         }
-                        else if(isa<BranchInst>(instruction)){
-                            Instruction *inst =  const_cast<Instruction *>(&instruction); // casting shenanigans
-                            BranchInst *brInst = dyn_cast<BranchInst>(inst); // Oh God, why ?????
+                        else if (isa<BranchInst>(instruction))
+                        {
+                            Instruction *inst = const_cast<Instruction *>(&instruction); // casting shenanigans
+                            BranchInst *brInst = dyn_cast<BranchInst>(inst);             // Oh God, why ?????
                             parseBinaryBranchInstruction(brInst, &acfgNode);
                         }
-                        else if(isa<SwitchInst>(instruction)){
-                            //TODO
+                        else if (isa<SwitchInst>(instruction))
+                        {
+                            // [TODO]
                         }
                     }
+
+                    idAcfgNode[acfgNode.getBlockId()] = acfgNode;
                 }
+                printEdgeList(edgeList);
             }
             return false;
-        }        
+        }
     };
 }
-
 
 char BasicBlockExtractionPass::ID = 0;
 
